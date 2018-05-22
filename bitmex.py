3de604e5ec5e5177870048035bda2e6d1b841756
MAZMEX <37592011+mazmex7@users.noreply.github.com>
2018-05-22 22:00:20 +0900

    Update library code to 1 file.

--- bitmex.py
+++ bitmex.py
@@ -1,13 +1,55 @@
 #!/usr/bin/env python
 
-from bravado.client import SwaggerClient
-from bravado.requests_client import RequestsClient
-from bravado.swagger_model import Loader
+try:
+    from urllib.parse import urlparse
+except ImportError:
+    from urlparse import urlparse
 
-from BitMEXAPIKeyAuthenticator import APIKeyAuthenticator
+import time, hashlib, hmac
+
+from bravado.client import SwaggerClient
+from bravado.requests_client import RequestsClient, Authenticator
+from bravado.swagger_model import Loader
 
 # swagger spec's formats to exclude. this help to avoid warning in your console.
 EXCLUDE_SWG_FORMATS = ['JSON', 'guid']
+
+class APIKeyAuthenticator(Authenticator):
+
+    def __init__(self, host, api_key, api_secret):
+        super(APIKeyAuthenticator, self).__init__(host)
+        self.api_key = api_key
+        self.api_secret = api_secret
+
+    def matches(self, url):
+        if 'swagger.json' in url:
+            return False
+        return True
+
+    def apply(self, r):
+        # 5s grace period in case of clock skew
+        expires = int(time.time() * 1000)
+        r.headers['api-expires'] = str(expires)
+        r.headers['api-key'] = self.api_key
+        prepared = r.prepare()
+        body = prepared.body or ''
+        url = prepared.path_url
+        r.headers['api-signature'] = self.generate_signature(self.api_secret, r.method, url, expires, body)
+        return r
+
+    def generate_signature(self, secret, verb, url, nonce, data):
+        parsedURL = urlparse(url)
+        path = parsedURL.path
+        if parsedURL.query:
+            path = path + '?' + parsedURL.query
+
+        nonce = str(nonce)
+        _message = verb + path + nonce + data
+
+        message = bytes(_message.encode('utf-8'))
+        secret = bytes(secret.encode('utf-8'))
+        
+        return hmac.new(secret, message, digestmod=hashlib.sha256).hexdigest()
 
 def bitmex(test=True, config=None, api_key=None, api_secret=None):
     # config options at http://bravado.readthedocs.io/en/latest/configuration.html
@@ -38,7 +80,6 @@ def bitmex(test=True, config=None, api_key=None, api_secret=None):
         return SwaggerClient.from_spec(spec_dict, origin_url=spec_uri, http_client=request_client, config=config)
     else:
         return SwaggerClient.from_spec(spec_dict, origin_url=spec_uri, http_client=None, config=config)
-
 
 # exclude some format from swagger json to avoid warning in API execution.
 def get_swagger_json(spec_uri, exclude_formats=[]):
